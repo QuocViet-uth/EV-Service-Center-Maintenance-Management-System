1ae95e0952acb0860eaf6e5cd736d6e3a962ceaf
ThanhDora-tech <thanhdq1647@ut.edu.vn>
2025-10-23 14:55:19 +0700

    tạo cấu trúc thư mục và test API

---
+++ src/main/java/com/evservice/maintenance/EvServiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.evservice.maintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvServiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvServiceBackendApplication.class, args);
+	}
+
+}
